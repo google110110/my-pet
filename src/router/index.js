15441df35bbeeb2af83793c142ec19300d07f697
google110110 <wxuanwxwx@gmail.com>
2020-04-06 11:46:31 +0800

    m11-46

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,8 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
+  mode :'hash',
   // base: process.env.BASE_URL,
   base: 'my-pet/dist',
   routes
